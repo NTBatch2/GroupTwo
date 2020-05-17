76cd0a96859aa746886b8f4be84265ad48af4145
rabia <rabia@10.0.0.109>
2020-05-17 11:51:50 -0400

    I add comment in new class

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -14,26 +14,6 @@ public class Demo {
 		System.out.println("Rabia add comid again");
 		//add more
 		
-		System.out.println("Rabia add comid again");  
-		
-		System.out.println("Good job everyone");
-		
-		System.out.println("Hello");
-		//hi
-		System.out.println(" Vio Changes here .....");
-		
-		//System.out.println("Well done!");
-		
-		// Try to review this one
-		System.out.println(" Any Other Updates Guys....? ");
-	     
-	    	 //i see we got it :)
-		System.out.println("We all should get 60$/h");
-		
-		System.out.println("Sunday");
-		
-		System.out.println("Rabia");
-		System.out.println("added by me....");
-	     }
+		//good morning from Rabia
 	}
-
+}
---
+++ src/noorteck/Rabia.java
@@ -0,0 +1,9 @@
+package noorteck;
+
+public class Rabia {
+	public static void main(String[] args) {
+		System.out.println("this is my second try for new class");
+		
+	}
+
+}
