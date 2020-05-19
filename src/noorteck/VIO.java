816740b1a9b02fe531049f530615a4e28e53dba8
alialbassam <alialbassam@alis-mbp.fios-router.home>
2020-05-19 00:27:56 -0400

    update from Ali

--- src/noorteck/VIO.java
+++ src/noorteck/VIO.java
@@ -5,6 +5,9 @@ public class VIO {
 	public static void main(String[] args) {
 		
 		System.out.println(" Guys can You see my new Class? ...");
-	}//hi
-
+	//hi
+	
+	// update from ali 
+	System.out.println("Ali");
+	}
 }
